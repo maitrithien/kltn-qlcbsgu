65e18eb5b8637409700b1e0ad41a2cec36fd4670
thanhtv91@gmail.com <thanhtv91@gmail.com@03a6d688-3fb4-b8db-912a-0cb9bfeea982>
2013-03-03 10:21:39 +0000

    search_advance

--- trunk/ kltn-qlcbsgu/app/models/can_bo_thong_tin.rb
+++ trunk/ kltn-qlcbsgu/app/models/can_bo_thong_tin.rb
@@ -13,13 +13,17 @@ class CanBoThongTin < ActiveRecord::Base
 	  end
   end
 
-  def self.search_advance(ma_cb,ho_ten,dan_toc,noi_o_hien_nay,so_cmnd,noi_dang_ky_ho_khau_thuong_tru)
-
-    if ma_cb.length>0
-      where('ma_cb = ? AND ho_ten LIKE ? AND dan_toc LIKE ? AND noi_o_hien_nay LIKE ? AND so_cmnd LIKE ? AND noi_dang_ky_ho_khau_thuong_tru LIKE ?',ma_cb,"%#{ho_ten}%","%#{dan_toc}%","%#{noi_o_hien_nay}%","%#{so_cmnd}%","%#{noi_dang_ky_ho_khau_thuong_tru}%")
+  def self.search_advance(ho_ten,ten_goi_khac,ton_giao,dan_toc,gioi_tinh,so_cmnd, noi_dang_ky_ho_khau_thuong_tru,que_quan,noi_o_hien_nay,so_bhxh,noi_sinh)
+    if ho_ten.length > 0 || ten_goi_khac.length > 0 ||ton_giao.length > 0 ||dan_toc.length > 0 ||gioi_tinh != nil ||so_cmnd.length > 0 || noi_dang_ky_ho_khau_thuong_tru.length > 0 ||que_quan.length > 0 ||noi_o_hien_nay.length > 0 ||so_bhxh.length > 0 ||noi_sinh.length > 0
+      if gioi_tinh!=nil
+        where('ho_ten LIKE ? AND ten_goi_khac LIKE ? AND ton_giao LIKE ? AND dan_toc LIKE ? AND gioi_tinh = ? AND so_cmnd LIKE ? AND noi_dang_ky_ho_khau_thuong_tru LIKE ? AND que_quan LIKE ? AND noi_o_hien_nay LIKE ? AND so_bhxh LIKE ? AND noi_sinh LIKE ?',"%#{ho_ten}%","%#{ten_goi_khac}%","%#{ton_giao}%","%#{dan_toc}%",gioi_tinh,"%#{so_cmnd}%","%#{noi_dang_ky_ho_khau_thuong_tru}%","%#{que_quan}%","%#{noi_o_hien_nay}%","%#{so_bhxh}%","%#{noi_sinh}%")
+      else
+        where('ho_ten LIKE ? AND ten_goi_khac LIKE ? AND ton_giao LIKE ? AND dan_toc LIKE ? AND so_cmnd LIKE ? AND noi_dang_ky_ho_khau_thuong_tru LIKE ? AND que_quan LIKE ? AND noi_o_hien_nay LIKE ? AND so_bhxh LIKE ? AND noi_sinh LIKE ?',"%#{ho_ten}%","%#{ten_goi_khac}%","%#{ton_giao}%","%#{dan_toc}%","%#{so_cmnd}%","%#{noi_dang_ky_ho_khau_thuong_tru}%","%#{que_quan}%","%#{noi_o_hien_nay}%","%#{so_bhxh}%","%#{noi_sinh}%")
+      end
     else
-      where('ho_ten LIKE ? AND dan_toc LIKE ? AND noi_o_hien_nay LIKE ? AND so_cmnd LIKE ? AND noi_dang_ky_ho_khau_thuong_tru LIKE ?',"%#{ho_ten}%","%#{dan_toc}%","%#{noi_o_hien_nay}%","%#{so_cmnd}%","%#{noi_dang_ky_ho_khau_thuong_tru}%")
+      where('ho_ten ="ten_goi_khac_ho_ten" ')
     end
+
   end
 
 end
