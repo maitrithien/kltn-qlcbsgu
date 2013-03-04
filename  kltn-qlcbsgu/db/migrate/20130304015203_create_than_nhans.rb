977e19ab01a229559dcebd50edddfaa2d702776d
maitrithien@gmail.com <maitrithien@gmail.com@03a6d688-3fb4-b8db-912a-0cb9bfeea982>
2013-03-04 17:24:22 +0000

    Thông tin thân nhân

---  kltn-qlcbsgu/db/migrate/20130304015203_create_than_nhans.rb
+++  kltn-qlcbsgu/db/migrate/20130304015203_create_than_nhans.rb
@@ -1,8 +1,9 @@
 class CreateThanNhans < ActiveRecord::Migration
   def self.up
-    add_column :than_nhans, :ho_ten, :string, :null => false
+
     create_table :than_nhans do |t|
       t.references :can_bo_thong_tin
+      t.string :ho_ten, :null => false
       t.string :quan_he_voi_cb, :unique_id => true
       t.integer :nam_sinh
       t.string :nghe_nghiep
@@ -10,9 +11,6 @@ class CreateThanNhans < ActiveRecord::Migration
       t.timestamps
     end
 
-    execute<<-SQL
-    ALTER TABLE thanh_nhans ADD UNIQUE(quan_he_voi_cb)
-    SQL
   end
 
   def self.down
---  kltn-qlcbsgu/db/migrate/20130304070317_add_ho_ten_column_to_than_nhans.rb
+++
@@ -1,5 +0,0 @@
-class AddHoTenColumnToThanNhans < ActiveRecord::Migration
-  def change
-    add_column :than_nhans, :ho_ten, :string, :null => false
-  end
-end
